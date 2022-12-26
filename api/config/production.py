ab3299d8c2e74c450a097f891a7e3e3c6c816149
Stephen Mutua <8377886+stephen2m@users.noreply.github.com>
2022-12-26 19:18:56 +0300

    update production settings

--- api/config/production.py
+++ api/config/production.py
@@ -5,8 +5,9 @@ from .common import Common
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+    DEBUG = False
 
     # Site
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = [".cloud.okteto.net"]
     INSTALLED_APPS += ("gunicorn", )
